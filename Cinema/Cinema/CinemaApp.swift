ecf3bd037e5b701c3df9dcadbbef9c7a9ec152d5
Jose96Luis <jose_luis1996@live.com.mx>
2024-03-06 14:31:25 -0600

    InitialCommit

---
+++ Cinema/Cinema/CinemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaApp.swift
+//  Cinema
+//
+//  Created by José Rodriguez Romero on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
